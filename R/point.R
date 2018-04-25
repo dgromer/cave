85140dbe2888457dd1bf06a6bf39c269f50957fd
Daniel Gromer <dgromer@mailbox.org>
2018-04-25 19:23:03 +0200

    Documentation

--- R/point.R
+++ R/point.R
@@ -5,6 +5,10 @@
 #' @param x numeric, x coordinate
 #' @param y numeric, y coordinate
 #' @return an object of class `point`
+#' @examples
+#' library(tidyverse)
+#' df <- data.frame(x = c(1, 2, 3, 4), y = c(4, 5, 2, 8))
+#' mutate(df, p = map2(x, y, point))
 #'
 #' @export
 point <- function(x, y)
@@ -23,6 +27,7 @@ point <- function(x, y)
   UseMethod("%--%", x)
 }
 
+#' @export
 `%--%.point` <- function(a, b)
 {
   sqrt((b$x - a$x) ^ 2 + (b$y - a$y) ^ 2)
